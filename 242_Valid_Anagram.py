336a75c273fac2f8a431e798cffcd79e8e81301d
deekshitharv <rvdeekshithabsc24@rvu.edu.in>
2026-01-30 10:57:03 +0530

    solution using binary search

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        
+        if len(s) != len(t):
+            return False
+    
+        count = {}
+        
+        for c in s:
+            if c in count:
+                count[c] += 1
+            else:
+                count[c] = 1
+            
+        for c in t:
+            if c not in count:
+                return False
+            
+            count[c] -= 1
+
+            if count[c] < 0:
+                return False
+                
+        return True
